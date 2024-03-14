b13e23dc3a5c0bf52406788a8c04988fb1e9df77
Anton Shaymardanov <antonnn1337@gmail.com>
2024-03-14 10:48:41 +0500

    v2

--- HANDLERS/user_private.py
+++ HANDLERS/user_private.py
@@ -1,18 +1,22 @@
 from aiogram import  types, Router
 from aiogram.filters import CommandStart, Command
-from info import txt1
+from info import иницт, bmk, start, bmz
 
 user_private_router = Router()
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer('Напишите "/menu" для того чтобы узнать о функционале бота😉')
+    await message.answer(start)
 
 
-@user_private_router.message(Command('menu'))
-async def as_cmd(message: types.Message):
-    await message.answer(txt1)
+@user_private_router.message(Command('БМЗ'))
+async def men_cmd(message: types.Message):
+    await message.answer(bmz)
 
 @user_private_router.message(Command('иницт'))
-async def vf_cmd(message: types.Message):
-    await message.answer('ицвильнихт')
+async def mf_cmd(message: types.Message):
+    await message.answer(иницт)
+
+@user_private_router.message(Command('БМК'))
+async def bmk_cmd(message: types.Message):
+    await message.answer(bmk)
